7547249e0c1e3cf7d1d2c06b0d3e444b307f7bda
niketa282 <niketa282@gmail.com>
2026-03-31 21:18:00 +0100

    Add definition for read_images() function

---
+++ vpr/dataset.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from PIL import Image
+import torchvision.transforms as T
+
+IMG_EXTENSIONS = {".jpg", ".jpeg", ".png", ".bmp", ".webp"}
+
+def read_images(db_dir="dataset/database", query_dir="dataset/queries", img_size=322):
+    db_paths = sorted(p for p in Path(db_dir).rglob("*") 
+                      if p.suffix.lower() in IMG_EXTENSIONS)
+    q_paths  = sorted(p for p in Path(query_dir).rglob("*") 
+                      if p.suffix.lower() in IMG_EXTENSIONS)
+    return q_paths, db_paths
